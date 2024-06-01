e857d725eb038c230aecf3cc46dafe745e026714
Eduardo Noronha <eduardo.noronhasampaio@gmail.com>
2024-06-01 17:05:56 -0300

    style(configuracaousuarioview): grid layouts have been added to organize user information

--- StreetEye-App/Views/Usuarios/ConfiguracaoUsuarioView.xaml.cs
+++ StreetEye-App/Views/Usuarios/ConfiguracaoUsuarioView.xaml.cs
@@ -12,32 +12,50 @@ public partial class ConfiguracaoUsuarioView : ContentPage
         viewModel = new UsuarioViewModel();
         BindingContext = viewModel;
 
+        btnBuscarCep.Pressed += OnButtonBuscarCepPressed;
+        btnAlterar.Pressed += OnButtonAlterarPressed;
+
         string login = Preferences.Get("UsuarioUsername", string.Empty);
         lblLogin.Text = $"{login}";
 
         string email = Preferences.Get("UsuarioEmail", string.Empty);
-        lblEmail.Text = $"{email}";
+        lblEmail.Text = $"Email: {email}";
 
         string telefone = Preferences.Get("UsuarioTelefone", string.Empty);
-        lblTelefone.Text = $"{telefone}";
+        lblTelefone.Text = $"Telefone: {telefone}";
 
         string cep = Preferences.Get("UsuarioCep", string.Empty);
-        lblCep.Text = $"{cep}";
+        lblCep.Text = $"Cep: {cep}";
 
         string endereco = Preferences.Get("UsuarioEndereco", string.Empty);
-        lblEndereco.Text = $"{endereco}";
+        lblEndereco.Text = $"Endereço: {endereco}";
 
         string numeroEndereco = Preferences.Get("UsuarioNumeroEndereco", string.Empty);
         lblNumeroEndereco.Text = $"{numeroEndereco}";
 
         string bairro = Preferences.Get("UsuarioBairro", string.Empty);
-        lblBairro.Text = $"{bairro}";
+        lblBairro.Text = $"Bairro: {bairro}";
 
         string cidade = Preferences.Get("UsuarioCidade", string.Empty);
         lblCidade.Text = $"{cidade}";
 
-        string uf = Preferences.Get("UsuarioUf", string.Empty);
-        lblUf.Text = $"{uf}";
+    }
+
+    private async void OnButtonBuscarCepPressed(object sender, EventArgs e)
+    {
+        // Animação de escala
+        await btnBuscarCep.ScaleTo(0.9, 50, Easing.Linear);
+        await Task.Delay(50);
+        await btnBuscarCep.ScaleTo(1, 50, Easing.Linear);
+
+    }
+
+    private async void OnButtonAlterarPressed(object sender, EventArgs e)
+    {
+        // Animação de escala
+        await btnAlterar.ScaleTo(0.9, 50, Easing.Linear);
+        await Task.Delay(50);
+        await btnAlterar.ScaleTo(1, 50, Easing.Linear);
 
     }
 }
